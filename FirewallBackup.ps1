eb1aa26fa10e13612017a1dcf1eda65ffad02f2c
Artem <111682257+miqblade@users.noreply.github.com>
2025-11-04 12:24:05 +0200

    Update firewall reset message to English

--- FirewallBackup.ps1
+++ FirewallBackup.ps1
@@ -1,3 +1,4 @@
 netsh advfirewall reset
 Set-NetFirewallProfile -Profile Domain,Private,Public -Enabled True
-Write-Host "Windows Firewall сброшен к настройкам по умолчанию."
+Write-Host "Windows Firewall reset to default settings."
+
